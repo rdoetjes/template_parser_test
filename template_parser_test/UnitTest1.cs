e6dbd8362d72855ae9d95d8c1d78cf0f89de7ef8
Raymond Doetjes <rdoetjes@phonax.com>
2020-04-23 08:24:28 +0200

    updated test to suite dictionary usage and generate test xml runtime

--- template_parser_test/UnitTest1.cs
+++ template_parser_test/UnitTest1.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using NUnit.Framework;
 using TemplateParser;
@@ -8,11 +9,49 @@ namespace Tests
     {
         public SourceXmlReader sr;
 
+        public string xml = @"<whatever>
+                                <configTemplates>
+                                  <dev>
+                                     <testConfig>
+                                        <value1>1</value1>
+                                        <value2>2</value2>
+                                        <value3>&amp;&lt;dev&gt;</value3>
+                                     </testConfig>
+                                  </dev>
+                                  <tst>
+                                        <testConfig>
+                                            <value1>3</value1>
+                                            <value2>1</value2>
+                                            <value3>&amp;&lt;tst&gt;</value3>
+                                        </testConfig>
+                                  </tst>
+                                  <acc>
+                                        <testConfig>
+                                            <value1>3</value1>
+                                            <value2>1</value2>
+                                            <value3>&amp;&lt;acc&gt;</value3>
+                                        </testConfig>
+                                  </acc>
+                                  <prd>
+                                        <testConfig>
+                                            <value1>3</value1>
+                                            <value2>1</value2>
+                                            <value3>&amp;&lt;prd&gt;</value3>
+                                        </testConfig>
+                                  </prd>
+                                </configTemplates>
+                            </whatever>
+                            ";
+
         [SetUp]
         public void Setup()
         {
             //there's a copy of the test.xml made into the in the Debug/bin directory
+
             string test = Path.Combine(TestContext.CurrentContext.TestDirectory, @"test.xml");
+
+            System.IO.File.WriteAllText(test, xml);
+
             sr = new SourceXmlReader(test, "dev/testConfig");
         }
 
@@ -21,12 +60,17 @@ namespace Tests
          {
             var a = sr.getKeyValuePair();
             Assert.AreEqual(3, a.Count);
-            Assert.AreEqual("value1", a[0].Key);
-            Assert.AreEqual("1", a[0].Value);
-            Assert.AreEqual("value2", a[1].Key);
-            Assert.AreEqual("2", a[1].Value);
-            Assert.AreEqual("value3", a[2].Key);
-            Assert.AreEqual("&<dev>", a[2].Value);
+
+            List<string> keys = new List<string>(a.Keys);
+
+            Assert.AreEqual(true, keys.Contains("value1"));
+            Assert.AreEqual("1", a["value1"]);
+
+            Assert.AreEqual(true, keys.Contains("value2"));
+            Assert.AreEqual("2", a["value2"]);
+
+            Assert.AreEqual(true, keys.Contains("value3"));
+            Assert.AreEqual("&<dev>", a["value3"]);
         }
 
         [Test]
@@ -35,12 +79,17 @@ namespace Tests
             sr.section = "/prd/testConfig";
             var a = sr.getKeyValuePair();
             Assert.AreEqual(3, a.Count);
-            Assert.AreEqual("value1", a[0].Key);
-            Assert.AreEqual("3", a[0].Value);
-            Assert.AreEqual("value2", a[1].Key);
-            Assert.AreEqual("1", a[1].Value);
-            Assert.AreEqual("value3", a[2].Key);
-            Assert.AreEqual("&<prd>", a[2].Value);
+
+            List<string> keys = new List<string>(a.Keys);
+
+            Assert.AreEqual(true, keys.Contains("value1"));
+            Assert.AreEqual("3", a["value1"]);
+
+            Assert.AreEqual(true, keys.Contains("value2"));
+            Assert.AreEqual("1", a["value2"]);
+
+            Assert.AreEqual(true, keys.Contains("value3"));
+            Assert.AreEqual("&<prd>", a["value3"]);
         }
     }
 }
